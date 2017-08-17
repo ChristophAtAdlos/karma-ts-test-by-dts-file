82256af253ba9344d7586a51c32de34fe8eec7a8
Christoph Kuhn <christoph.kuhn@adlos.com>
2017-08-17 10:14:01 +0200

    feat(testing) initial commit of the spec file main.spec.ts.

---
+++ src/main.spec.ts
@@ -0,0 +1,20 @@
+/**
+ * @class main.spec.ts
+ *
+ * @classdesc
+ *
+ * @overview
+ *
+ * @since 2017/08/17
+ * @author Christoph Kuhn <christoph.kuhn@adlos.com>
+ */
+
+
+import { myLib } from "./lib/myLib";
+
+QUnit.test("myLib", (assert) => {
+    const multiplicator = new myLib.test.MyClass(5);
+    assert.equal(multiplicator.getStaticMultiplicand(), 3, "The static multiplicand is '3'.");
+    assert.equal(multiplicator.getMultiplicand(), 5, "The variable multiplicand is '5'.");
+    assert.equal(multiplicator.getProduct(), 15, "The product of this multiplicands is '15'.");
+});
