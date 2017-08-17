ef4cae8705c28f2c64d102c273dececcbd0958cb
Christoph Kuhn <christoph.kuhn@adlos.com>
2017-08-17 10:13:36 +0200

    feat(code) initial commit of the entry script main.ts.

---
+++ src/main.ts
@@ -0,0 +1,23 @@
+/**
+ * @class main
+ *
+ * @classdesc
+ *
+ * @overview
+ *
+ * @since 2017/08/17
+ * @author Christoph Kuhn <christoph.kuhn@adlos.com>
+ */
+
+
+import { myLib } from "./lib/myLib";
+
+const multiplicator = new myLib.test.MyClass(5);
+console.log(
+    "The product of",
+    multiplicator.getMultiplicand(),
+    "and",
+    multiplicator.getStaticMultiplicand(),
+    "is",
+    multiplicator.getProduct(),
+);
